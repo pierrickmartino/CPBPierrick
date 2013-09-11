0f52459af4483966becd8fc7dda98f0165475b77
Pierrick Martino <pierrick.martino@gmail.com>
2013-09-11 09:39:44 +0200

    Start Project : Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CPBPierrick::Application.config.secret_token = '720c5a021f9d69c52d892ddcf72aa1239f870d8bb36a018678484e1b79bca93cafa3e566fd63dee057e39c652d59b57b7861b72dbd2a76b5ff963acf0eb920c8'
